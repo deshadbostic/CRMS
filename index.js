ea0acc0f84ec24d38eac66d14aff7d44fe122438
KalebLaw22 <kaleb.l.lawrence@hotmail.com>
2023-04-12 15:05:52 -0400

    Updated port number to 5500

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const {app} = require('./server')
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5500;
 console.log(PORT);
 app.listen(PORT,() =>console.log(`server started ${PORT}`));

70df84101c559811779127c6cf532c99231d5a36
bloodlus69 <deshad12@gmail.com>
2023-04-13 11:39:56 -0300

    implement working

--- public/js/rental.js
+++ public/js/rental.js
@@ -1,17 +1,14 @@
-function loadrentals(){
-    $.ajax({
+ function loadrentals(){
+     return JSON.parse($.ajax({
         url:"/rentals/loadrentals",
         type:"GET",
+        async:false,
         contentType:"application/json; charset=utf-8",
         dataType:"json",
-        success: function(response){
-             console.log(response);
-       return response;
+    }).responseText);
 }
-    });
-}
-function implementrentalVehRen(){
-    var loadrent = loadrentals()
+async function implementrentalVehRen(){
+    var loadrent = await loadrentals()
     let select = document.getElementById("Veh_Ren_His"); 
     select.innerHTML="";
      console.log( loadrent[0]);
@@ -39,23 +36,25 @@ function implementrentalVehRen(){
           }
 
 
-          function implementrentalRenHis(){
-            var loadrent = Array.from(loadrentals());
-           var rr = document.getElementsByClassName("field");
-           var total_num ;
-           var total_bal;
-           console.log(loadrent);
-           for(let i = 0; i <  loadrent.length; i++) {
-             rr[i].textContent =  loadrent[i].rental_Period;
-             rr[i].textContent =  loadrent[i].rental_Rate;
-            total_num++;
-            total_bal += loadrent[i].amountCharged;
+            let implementrentalRenHis= async ()=>{
+            var loadrent =  await loadrentals();       
+              var rr = document.getElementsByClassName("field");
+              var total_num=0 ;
+              var total_bal =0;
+              console.log(loadrent);
+let x=0;
+              for(let i = 0; i <  loadrent.length; i++) {
+                rr[i++].value =  loadrent[x].rental_Period;
+                rr[i].value =  loadrent[x].rental_Rate;
+               total_num++;
+               total_bal += loadrent[x].amountCharged;
+                x++;
+        
+               //select.appendChild(el);
+           }
 
-            let el = document.createElement("option");
-            el.textContent = opt;
-            el.value =i;
-            select.appendChild(el);
-        }
+           //set total text box 
+            //set number of
               }
 
 

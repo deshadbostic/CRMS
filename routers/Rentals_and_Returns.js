040eeccef08f87cced584e0f984b3694bd2646c5
TurboTurtle786 <zaccaismall7@gmail.com>
2023-04-12 20:17:10 -0400

    commit

--- routers/Rentals_and_Returns.js
+++ routers/Rentals_and_Returns.js
@@ -53,7 +53,7 @@ router.post('/addrental', function (req, res) {
     const Cust_ID = req.body.Cus_ID;
     
     
-    const Veh_Vin = req.body.vin;
+    const Veh_Vin = req.body.VIN;
     
 //Rental Agreement
     const rentalAgreementTerms = req.body.rental_terms;

991563dcae354e8e9ee4e34f5f78891875a930f8
TurboTurtle786 <zaccaismall7@gmail.com>
2023-04-13 13:19:56 -0400

    commit

--- public/js/rental.js
+++ public/js/rental.js
@@ -1,3 +1,5 @@
+//const { loadcustomers} = require("./customer.js");
+
  function loadrentals(){
      return JSON.parse($.ajax({
         url:"/rentals/loadrentals",
@@ -21,14 +23,18 @@ async function implementrentalVehRen(){
     }
       }
 
-      function implementrentalCusRen(){
-        var loadrent = loadrentals();
+      let implementrentalCusRen= async ()=>{
+        var loadrent = await loadrentals();
        var rr = document.getElementsByClassName("field");
+       console.log(loadrent);
+       let x = 0;
         for(let i = 0; i <  loadrent.length; i++) {
-        rr[i].textContent = loadrent[i].rental_Date;
-        rr[i].textContent = loadrent[i].rental_Rate;
-        rr[i].textContent = loadrent[i].additional_Fees;
-        rr[i].textContent = loadrent[i].Veh_Vin;
+        rr[i++].value = loadrent[x].rental_Date;
+        rr[i].value = loadrent[x].Veh_Vin;
+        rr[i].value = loadrent[x].rental_Rate;
+        rr[i].value = loadrent[x].additional_Fees;
+        
+        x++;
             
         }
           }
@@ -102,4 +108,6 @@ async function implementrentalVehRen(){
                             select.appendChild(el);
                         }
                           }
+                     
+                          exports = { loadrentals};
                         
